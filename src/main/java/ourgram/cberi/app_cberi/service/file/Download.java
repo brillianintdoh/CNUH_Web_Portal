a98995c0bc3fd6a638700332e529affcc981c614
brillianintdoh <palanghwi@gmail.com>
2024-07-24 22:48:45 +0900

    JdbcTemplate를 DBGet 및 DBedit 인스턴스로 대체하기 위해 Download 및 Upload 클래스를 리팩터했습니다

--- src/main/java/ourgram/cberi/app_cberi/service/file/Download.java
+++ src/main/java/ourgram/cberi/app_cberi/service/file/Download.java
@@ -8,23 +8,23 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ourgram.cberi.app_cberi.security.db.DBGet;
 import ourgram.cberi.app_cberi.security.db.DBUser;
 import ourgram.cberi.app_cberi.security.UserDB;
 
 @RestController
 public class Download {
-    private JdbcTemplate jdbc;
     private ResourceLoader load;
     private DBUser db;
+    private DBGet get;
 
     @Autowired
-    public Download(JdbcTemplate jdbc, ResourceLoader load, DBUser db) {
-        this.jdbc = jdbc;
+    public Download(DBGet get, ResourceLoader load, DBUser db) {
+        this.get = get;
         this.load = load;
         this.db = db;
     }
@@ -38,10 +38,9 @@ public class Download {
             id = db.getID(username);
         }
         HttpHeaders head = new HttpHeaders();
-        Integer count = jdbc.queryForObject("SELECT COUNT(*) FROM profile_img WHERE user_id=?", Integer.class, id);
 
-        if(count > 0) {
-            String path = jdbc.queryForObject("SELECT file_path FROM profile_img WHERE user_id=?", String.class, id);
+        if(get.isProfile(id)) {
+            String path = get.getProfilePath(id);
             Resource file = load.getResource("file:" + path);
             head.add("Content-Type", Files.probeContentType(file.getFile().toPath()));
             return new ResponseEntity<>(file, head, HttpStatus.OK);
--- src/main/java/ourgram/cberi/app_cberi/service/file/Upload.java
+++ src/main/java/ourgram/cberi/app_cberi/service/file/Upload.java
@@ -2,18 +2,18 @@ package ourgram.cberi.app_cberi.service.file;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import ourgram.cberi.app_cberi.security.db.DBEdit;
+import ourgram.cberi.app_cberi.security.db.DBGet;
 import ourgram.cberi.app_cberi.security.db.DBUser;
 import ourgram.cberi.app_cberi.security.UserDB;
 
@@ -21,13 +21,15 @@ import ourgram.cberi.app_cberi.security.UserDB;
 @RequestMapping("/upload")
 public class Upload {
     public static String directory = "/media/wheedo/ubuntu/resources/web/cberi/profile/";
-    private JdbcTemplate jdbc;
     private DBUser db;
+    private DBGet get;
+    private DBEdit edit;
 
     @Autowired
-    public Upload(JdbcTemplate jdbc, DBUser db) {
-        this.jdbc = jdbc;
+    public Upload(DBGet get, DBUser db, DBEdit edit) {
+        this.get = get;
         this.db = db;
+        this.edit = edit;
     }
 
     @PostMapping("/profile")
@@ -37,13 +39,12 @@ public class Upload {
         String id = UserDB.getId(token);
         head.add("Content-Type","text/html; charset=UTF-8");
 
-        List<String> list = jdbc.queryForList("SELECT user_id FROM profile_img", String.class);
         String path = directory + db.getUsername(id) + ".png";
         try {
-            if(list.contains(id)) {
-                path = jdbc.queryForObject("SELECT file_path FROM profile_img WHERE user_id=?", String.class, id);
+            if(get.isProfileImg(id)) {
+                path = get.getProfilePath(id);
             }else {
-                jdbc.update("INSERT INTO profile_img (user_id, file_path) VALUES (?,?)", id, path);
+                edit.setProfileImg(id, path);
             }
             File file = new File(path);
             img.transferTo(file);
