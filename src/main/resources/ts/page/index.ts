55ce2517d599255f8a92e7391d9f0654829506ae
brillianintdoh <palanghwi@gmail.com>
2024-09-06 16:14:20 +0900

    update

--- src/main/resources/ts/page/index.ts
+++ src/main/resources/ts/page/index.ts
@@ -1,16 +1,18 @@
 
 export function index() {
     const appBtn = document.getElementById("appBtn_install") as HTMLElement;
-    const app_install = (window as any).appInstall as Window;
-
-    if(app_install) {
-        appBtn.style.display = 'block';
-        appBtn.addEventListener("click", () => {
-            if(app_install) {
-                app_install.prompt();
-            }else {
-                alert("현재 사용자님의 브라우저는 PWA를 지원하지 않습니다");
-            }
-        });
-    }
+    setTimeout(() => {
+        const app_install = (window as any).appInstall as Window;
+        
+        if(app_install) {
+            appBtn.style.display = 'block';
+            appBtn.addEventListener("click", () => {
+                if(app_install) {
+                    app_install.prompt();
+                }else {
+                    alert("현재 사용자님의 브라우저는 PWA를 지원하지 않습니다");
+                }
+            });
+        }
+    },800);
 }
